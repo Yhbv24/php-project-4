f50bc2e2ad58db43a0212663d2731d20b7eaefbc
Ash Laidlaw <ashlaidlaw@Ashs-MacBook-Pro.local>
2017-03-03 11:50:18 -0800

    Add tables for stores and brands, delete all button, and delete individual stores buttons

--- app/app.php
+++ app/app.php
@@ -22,17 +22,52 @@
 
     $app->get("/", function() use ($app) { // Route to the home page
         $brands = Brand::getAll();
-        $store = Store::getAll();
+        $stores = Store::getAll();
 
         return $app["twig"]->render("index.html.twig", array("brands" => $brands, "stores" => $stores));
     });
 
-    // ***** Post routs *****
+    // ***** Post routes *****
+
+    $app->post("/add_store", function() use ($app) { // Add a store
+        $name = filter_var($_POST["store_name"], FILTER_SANITIZE_MAGIC_QUOTES);
+        $phone_number = filter_var($_POST["phone_number"], FILTER_SANITIZE_MAGIC_QUOTES);
+        $street = filter_var($_POST["street"], FILTER_SANITIZE_MAGIC_QUOTES);
+        $city = filter_var($_POST["city"], FILTER_SANITIZE_MAGIC_QUOTES);
+        $state = filter_var($_POST["state"], FILTER_SANITIZE_MAGIC_QUOTES);
+        $zip = filter_var($_POST["zip"], FILTER_SANITIZE_MAGIC_QUOTES);
+        $store = new Store($name, $phone_number, $street, $city, $state, $zip);
+        $store->save();
+
+        return $app->redirect("/");
+    });
+
+    $app->post("/add_brand", function() use ($app) { // Add a brand
+        $name = filter_var($_POST["brand_name"], FILTER_SANITIZE_MAGIC_QUOTES);
+        $brand = new Brand($name);
+        $brand->save();
+
+        return $app->redirect("/");
+    });
 
     // ***** Delete routes *****
 
-    // ***** Patch routes *****
+    $app->delete("/delete_all", function() use ($app) { // Delete all information from database
+        Brand::deleteAll();
+        Store::deleteAll();
+        $GLOBALS["DB"]->exec("DELETE FROM stores_brands;");
 
+        return $app->redirect("/");
+    });
+
+    $app->delete("/delete_store/{id}", function($id) use ($app) { // Delete individual store
+        $store = Store::find($id);
+        $store->delete();
+
+        return $app->redirect("/");
+    });
+
+    // ***** Patch routes *****
 
     return $app;
 ?>
